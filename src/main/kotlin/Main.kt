e7999d94430731c975e34d8418a22f166b99584c
dannysteinbrecher <danny.steinbrecher@outlook.com>
2023-11-21 14:35:59 +0100

    Mainkt ausgeblendet und kommentiert, da es kein ausführbarer Code mehr wird

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,15 @@
-import domain.Product
-import io.github.cdimascio.dotenv.dotenv
-
-suspend fun main() {
-    val dotenv = dotenv()
-    val email = dotenv["EMAIL"]
-    val password = dotenv["PASSWORD"]
-
-    val bringApi = BringApi(email = email, password = password)
-    println(bringApi.getPurchaseList())
-    bringApi.addProductToList(Product("JsonÄpfel111111111", "111111111"))
-    println(bringApi.getPurchaseList())
-
-}
+//import domain.Product
+//import io.github.cdimascio.dotenv.dotenv
+//
+//// da es hier bald zu einer Lib wird, ist die Main nur noch für einfache Testt Zwecke. Wird in Zukunft gelöscht.
+//suspend fun main() {
+//    val dotenv = dotenv()
+//    val email = dotenv["EMAIL"]
+//    val password = dotenv["PASSWORD"]
+//
+//    val bringApi = BringApi(email = email, password = password)
+//    println(bringApi.getPurchaseList())
+//    bringApi.addProductToList(Product("JsonÄpfel111111111", "111111111"))
+//    println(bringApi.getPurchaseList())
+//
+//}
